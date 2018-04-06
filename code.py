7e245d60dc0a12c4bf4941fddd48d87278698ed8
rajukoushik <g.rajukoushik@gmail.com>
2018-04-06 12:10:31 +0530

    Added plotting of the k means clustering

--- code.py
+++ code.py
@@ -2,6 +2,30 @@ from sklearn.cluster import KMeans
 import numpy as np
 from numpy import array
 import collections
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../tools/")
+import pylab as pl
+
+
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+    """ some plotting code designed to help you visualize your clusters """
+
+    ### plot each cluster with a different color--add more colors for
+    ### drawing more than five clusters
+    colors = ["b", "c", "k", "m", "g"]
+    for ii, pp in enumerate(pred):
+        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+
+    ### if you like, place red stars over points that are POIs (just for funsies)
+    if mark_poi:
+        for ii, pp in enumerate(pred):
+            if poi[ii]:
+                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+    plt.xlabel(f1_name)
+    plt.ylabel(f2_name)
+    plt.savefig(name)
+    plt.show()
 
 
 def num(s):
@@ -81,13 +105,11 @@ print(len(label_list))
 counter=collections.Counter(label_list)
 print(counter)
 
-color = np.random.rand(cluster_num)
-
-
-
-
-
-
+pred = kmeans.predict(numArray)
+poi = "poi"
 
 
 print (kmeans.cluster_centers_)
+
+pl.scatter(numArray[:,0],numArray[:,0],c='black')
+pl.show()
